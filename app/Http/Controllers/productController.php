ddf1a9887b2b378ba50e6d52e69d7abe5f674d91
beniz-el <59539157+beniz-el@users.noreply.github.com>
2020-07-22 01:34:18 +0100

    productController

---
+++ app/Http/Controllers/productController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\product;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
+
+class productController extends Controller
+{
+     public function store(Request $request){
+        
+        $store = new product();
+    	$store->name = $request->input('name');
+    	$store->description = $request->input('description');
+    	$store->price = $request->input('price');
+    	$store->category = $request->input('category');     
+        
+        if(request()->hasFile('file')){
+            $file = $request->file('file');
+            $extension = $file->getClientOriginalExtension();
+            $filename = time() . '.' . $extension;
+            $file->move('uploads/images/', $filename);
+            $store->image = $filename;
+        }
+        else{
+            return $request;
+            $store->image = '';
+        }
+
+    	$store->save();
+      
+        $product = DB::table('products')->whereNotNull('name')->orderBy('name','asc')->orderBy('price','asc')->get();
+        return view('products', ['products'=> $product]);   
+    }
+
+      public function search(Request $request){
+        $search=$request->get('search');
+        $product = DB::table('products')->where('category', 'like' ,'%' . $search . '%')->paginate(5);
+        return view('products', ['products'=> $product]);
+    }
+
+      public function supprimer($id){
+         product::find($id)->delete();
+         $product = DB::table('products')->whereNotNull('name')->orderBy('name','asc')->get();;
+        return view('products', ['products'=> $product]); 
+    }
+}
