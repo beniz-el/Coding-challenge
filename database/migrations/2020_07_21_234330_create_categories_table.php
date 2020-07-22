812b72bed95ab65890a7231185c3f6563443dfa4
beniz-el <59539157+beniz-el@users.noreply.github.com>
2020-07-22 22:44:08 +0100

    Update 2020_07_21_234330_create_categories_table.php

--- database/migrations/2020_07_21_234330_create_categories_table.php
+++ database/migrations/2020_07_21_234330_create_categories_table.php
@@ -14,10 +14,11 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-           $table->bigIncrements('id');
-            $table->string('name');
+           $table->bigIncrements('id')->unsigned();
+            $table->string('name')->unique();
             $table->string('category');
             $table->timestamps();
+
         });
     }
 
